aba76e4cd006ec0b89743d1f809db67c661a4544
nshuti <nshutikope@gmail.com>
2026-04-24 20:30:00 +0530

    add useReducers component

---
+++ react-hooks/src/useReducers.tsx
@@ -0,0 +1,32 @@
+import { useReducer } from "react"
+
+export default function useReducers() {
+
+    const initialState = { count : 0 };
+
+    function Reducer(state, action) {
+
+        if (action.type === "increment") {
+
+           return {count: state.count + 1 }           
+        }
+        else {
+           return { count: state.count - 1 };           
+
+        }
+    }
+    const [state, dispatch] = useReducer( Reducer, initialState);
+
+    return (
+      <>
+        <p>{initialState.count}</p>
+        <button onClick={() => dispatch({ type: "increment" })}>
+          increment
+        </button>
+        <button onClick={() => dispatch({ type: "decrement" })}>
+          increment
+        </button>
+        
+      </>
+    );
+}
