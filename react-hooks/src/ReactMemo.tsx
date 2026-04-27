ef12d256464bffc1111d60cabe0d236b3e88bb34
nshuti <nshutikope@gmail.com>
2026-04-27 12:00:00 +0200

    push on this date April 27, 2026

---
+++ react-hooks/src/ReactMemo.tsx
@@ -0,0 +1,25 @@
+/** @format */
+
+import { useState } from "react";
+
+import Todos from "./Todos";
+
+export default function ReactMemo() {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(["todo 1", "todo 2"]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+
+  return (
+    <>
+      <Todos todos={todos} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
